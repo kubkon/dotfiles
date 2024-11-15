62c57ae40ec16ab7daf9f2559970ed34d8b6b63c
Jakub Konka <kubkon@jakubkonka.com>
2024-11-15 15:16:26 +0100

    nixos: clean up nixvim config

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -218,6 +218,60 @@ in {
         servers = {
           zls.enable = true;
         };
+        keymaps = {
+          diagnostic = {
+            "<leader>E" = "open_float";
+            "[" = "goto_prev";
+            "]" = "goto_next";
+            "<leader>do" = "setloclist";
+          };
+          lspBuf = {
+            "K" = "hover";
+            "gD" = "declaration";
+            "gd" = "definition";
+            "gr" = "references";
+            "gI" = "implementation";
+            "gy" = "type_definition";
+            "<leader>ca" = "code_action";
+            "<leader>cr" = "rename";
+            "<leader>wl" = "list_workspace_folders";
+            "<leader>wr" = "remove_workspace_folder";
+            "<leader>wa" = "add_workspace_folder";
+            "<C-k>" = "signature_help";
+          };
+        };
+        preConfig = ''
+          vim.diagnostic.config({
+              virtual_text = false,
+              signs = true,
+              update_in_insert = true,
+              underline = true,
+              severity_sort = false,
+              float = {
+                  border = 'rounded',
+                  source = 'always',
+                  header = "",
+                  prefix = "",
+              },
+          })
+
+          vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+            vim.lsp.handlers.hover,
+            {border = 'rounded'}
+          )
+
+          vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+            vim.lsp.handlers.signature_help,
+            {border = 'rounded'}
+          )
+        '';
+        postConfig = ''
+          local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+          for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+          end
+        '';
       };
 
       cmp = {
@@ -310,72 +364,6 @@ in {
       if vim.g.have_nerd_font then
         require('nvim-web-devicons').setup {}
       end
-    '';
-
-    extraConfigLua = ''
-      -- LSP Diagnostics Options Setup 
-      local sign = function(opts)
-        vim.fn.sign_define(opts.name, {
-          texthl = opts.name,
-          text = opts.text,
-          numhl = ""
-        })
-      end
-
-      sign({ name = 'DiagnosticSignError', text = ' ' })
-      sign({ name = 'DiagnosticSignWarn', text = ' ' })
-      sign({ name = 'DiagnosticSignHint', text = '󰌶 ' })
-      sign({ name = 'DiagnosticSignInfo', text = ' ' })
-
-      vim.diagnostic.config({
-          virtual_text = false,
-          signs = true,
-          update_in_insert = true,
-          underline = true,
-          severity_sort = false,
-          float = {
-              border = 'rounded',
-              source = 'always',
-              header = "",
-              prefix = "",
-          },
-      })
-
-      vim.cmd([[
-      set signcolumn=yes
-      autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-      ]])
-
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-          end, opts)
-        end,
-      })
     '';
   };
 
